d1aa1a5a3a3c0ad139b801cf2923c179a0520dbb
Maciek <miraxlesg@gmail.com>
2023-01-30 16:51:52 +0100

    styling changes

--- script.js
+++ script.js
@@ -1,95 +1,134 @@
-$(document).ready(()=>{
-    // let width = $(".row").first().css("width")
-    // $(".vertical-line").css("width", width)
-    // const mediaQuery = window.matchMedia('(min-width: 768px)')
-    // mediaQuery.addEventListener("change", () => {
-    //     $(".vertical-line").css("width", width)
-    // })
+let nextMoveX = true;
+let complete = false;
+let moves = 0;
+
+$(document).ready(() => {
+    const finish = () => {
+        complete = true;
+        $('.col.square').css('cursor', 'default');
+        $('button.restart').css('display', 'block');
+        if (nextMoveX) {
+            $('.winning-line').css('display', 'block');
+            $('.winning-line').css('background-color', 'var(--clr-blue)');
+            $('h2').html('Player 2 (O) has won!');
+        } else {
+            $('.winning-line').css('display', 'block');
+            $('.winning-line').css('background-color', 'var(--clr-red)');
+            $('h2').html('Player 1 (X) has won!');
+        }
+    };
 
     const check = (num1, num2, num3) => {
-        if($(".col").eq(num1).html().length > 0 && $(".col").eq(num1).html() == $(".col").eq(num2).html() && $(".col").eq(num2).html() == $(".col").eq(num3).html()) {
-            complete = true
-            $(".col").css("opacity", 0.2);
-            $(".col").eq(num1).css("opacity", 1);
-            $(".col").eq(num2).css("opacity", 1);
-            $(".col").eq(num3).css("opacity", 1);
-            $(".col").css("cursor", "default");
-            $("#move").text("Play again");
-            $("#move").css("cursor", "pointer");
-            if(!isX) {
-                $("#result").text("Player 1 (X) has won!");
-                $("#move").css("background-color", "var(--clr-blue)");
+        if (($('.col.square').children('.cross').eq(num1).css('opacity') == 1 && $('.col.square').children('.cross').eq(num2).css('opacity') == 1 && $('.col.square').children('.cross').eq(num3).css('opacity') == 1) || ($('.col.square').children('.circle').eq(num1).css('opacity') == 1 && $('.col.square').children('.circle').eq(num2).css('opacity') == 1 && $('.col.square').children('.circle').eq(num3).css('opacity') == 1)) {
+            finish();
+            let colHeight = $('.col.square').css('height');
+            let colWidth = colHeight;
+
+            // horizontal line
+            if (num1 + 1 == num2) {
+                $('.winning-line').css('transform', 'rotate(0deg)');
+                $('.winning-line').css('margin-left', '0');
+                $('.winning-line').css('height', '10px');
+                $('.winning-line').css('width', '100%');
+                // first row
+                if (num1 == 0) $('.winning-line').css('margin-top', `calc(${colHeight}/2 - 10px)`);
+                // second row
+                else if (num1 == 3) $('.winning-line').css('margin-top', 'calc(50% - 5px)');
+                // third row
+                else $('.winning-line').css('margin-top', `calc(${colHeight}*2 + ${colHeight}/2 + 20px)`);
             }
+
+            // vertical line
+            else if (num1 + 3 == num2) {
+                $('.winning-line').css('transform', 'rotate(0deg)');
+                $('.winning-line').css('height', '100%');
+                $('.winning-line').css('width', '10px');
+                $('.winning-line').css('margin-top', '0');
+                // first column
+                if (num1 == 0) $('.winning-line').css('margin-left', `calc(${colWidth}/2 - 10px)`);
+                // second row
+                else if (num1 == 1) $('.winning-line').css('margin-left', `calc(50% - 5px)`);
+                // third row
+                else $('.winning-line').css('margin-left', `calc(${colWidth}*2 + ${colWidth}/2 + 20px)`);
+            }
+
+            // diagonal line
             else {
-                $("#result").text("Player 2 (O) has won!");
-                $("#move").css("background-color", "var(--clr-orange)");
+                $('.winning-line').css('margin-top', 'calc(50% - 5px)');
+                $('.winning-line').css('margin-left', '-20%');
+                $('.winning-line').css('height', '10px');
+                $('.winning-line').css('width', '140%');
+                // from left top to bottom right corner
+                if (num1 == 0) $('.winning-line').css('transform', 'rotate(45deg)');
+                // from right top to bottom left corner
+                else $('.winning-line').css('transform', 'rotate(-45deg)');
             }
         }
-    }
+    };
 
-    let isX = true
-    let complete = false
-    let counter = 0
-
-    $(".col").hover((e)=>{
-        if($(e.target).html().length == 0 && !complete && e.target.tagName.toUpperCase() == 'DIV') {
-            if(isX)
-                $(e.target).css("background-color", "var(--clr-blue)");
-            else
-            $(e.target).css("background-color", "var(--clr-orange)");
-        }
-    }, (e)=>{
-        if(e.target.tagName.toUpperCase() == 'DIV')
-            $(e.target).css("background-color", "var(--clr-gray)");
-    }).click((e)=>{
-        if($(e.target).html().length > 0 || complete || e.target.tagName.toUpperCase() == 'I')
-            return
-        if(isX) {
-            $(e.target).append($("<i class='bi bi-x-lg'></i>"));
-            $("#move").text("Player 2 (O) turn");
-            $("#move").css("background-color", "var(--clr-orange)")
-            isX = false;
-        }
-        else {
-            $(e.target).append($("<i class='bi bi-circle'></i>"));
-            $("#move").text("Player 1 (X) turn");
-            $("#move").css("background-color", "var(--clr-blue)")
-            isX = true;
-        }
-        $(e.target).css("background-color", "var(--clr-gray)");
-        $(e.target).css("cursor", "default");
-        counter++;
-        for(let i=0; i<3; i++) {
-            if(complete)
-                break;
-            check(i*3, i*3+1, i*3+2); // check rows
-            check(i, i+3, i+6); // check columns
-        }
-        if(!complete) {
-            check(0, 4, 8); // check diagonal 1
-            check(2, 4, 6); // check diagonal 2
-        }
-        if(counter == 9 && !complete) {
-            $("#result").text("Tie.");
-            $("#move").text("Play again");
-            $("#move").css("cursor", "pointer");
-            complete = true
-            $(".col").css("cursor", "default");
-        }
-    });
-
-    $("#move").click(()=>{
-        if($("#move").css("cursor") == "pointer") {
-            $("#move").css("cursor", "default");
-            $("#move").text("Player 1 (X) turn");
-            $("#move").css("background-color", "var(--clr-blue)");
-            $(".col").html("");
-            $(".col").css("opacity", 1)
-            $(".col").css("cursor", "pointer");
-            $("#result").text("");
-            isX = true;
-            counter = 0;
-            complete = false;
-        }
+    $('.col.square')
+        .hover(
+            (e) => {
+                if (!$(e.currentTarget).hasClass('used') && !complete) {
+                    if (nextMoveX) {
+                        $(e.currentTarget).children('.cross').css('display', 'block');
+                        $(e.currentTarget).children('.cross').css('opacity', '0.5');
+                        $(e.currentTarget).children('.circle').css('display', 'none');
+                    } else {
+                        $(e.currentTarget).children('.cross').css('display', 'none');
+                        $(e.currentTarget).children('.circle').css('display', 'block');
+                        $(e.currentTarget).children('.circle').css('opacity', '0.5');
+                    }
+                }
+            },
+            (e) => {
+                if (!$(e.currentTarget).hasClass('used') && !complete) {
+                    $(e.currentTarget).children('.circle').css('display', 'none');
+                    $(e.currentTarget).children('.cross').css('display', 'none');
+                }
+            }
+        )
+        .click((e) => {
+            if (!$(e.currentTarget).hasClass('used') && !complete) {
+                moves++;
+                $(e.currentTarget).addClass('used');
+                $(e.currentTarget).css('cursor', 'default');
+                if (nextMoveX) {
+                    $(e.currentTarget).children('.cross').css('display', 'block');
+                    $(e.currentTarget).children('.cross').css('opacity', '1');
+                    $(e.currentTarget).children('.circle').css('display', 'none');
+                    $('h2').html('Player 2 (O) move');
+                } else {
+                    $(e.currentTarget).children('.cross').css('display', 'none');
+                    $(e.currentTarget).children('.circle').css('display', 'block');
+                    $(e.currentTarget).children('.circle').css('opacity', '1');
+                    $('h2').html('Player 1 (X) move');
+                }
+                nextMoveX = !nextMoveX;
+                if (moves == 9) $('h2').html('Tie!');
+                for (let i = 0; i < 3; i++) {
+                    if (complete) break;
+                    check(i * 3, i * 3 + 1, i * 3 + 2); // check rows
+                    check(i, i + 3, i + 6); // check columns
+                }
+                if (!complete) {
+                    check(0, 4, 8); // check diagonal 1
+                    check(2, 4, 6); // check diagonal 2
+                }
+            }
+        });
+    $('button.restart').click(() => {
+        moves = 0;
+        complete = false;
+        if (nextMoveX) $('h2').html('Player 1 (X) move');
+        else $('h2').html('Player 2 (O) move');
+        $('.col.square').children('.cross').css('display', 'none');
+        $('.col.square').children('.circle').css('display', 'none');
+        $('.col.square').children('.cross').css('opacity', '0');
+        $('.col.square').children('.circle').css('opacity', '0');
+        $('.col.square').removeClass('used');
+        $('.col.square').css('cursor', 'pointer');
+        $('.winning-line').css('display', 'none');
+        $('button.restart').css('display', 'none');
     });
 });
